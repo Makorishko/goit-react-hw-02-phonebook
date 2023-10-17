1bdc90c13b4f50813ae238b6f4af84591654692d
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-10-17 16:41:05 +0200

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ContactForm } from './form';
+import { Filter } from './filter';
+import { Component } from 'react';
+import { ContactList } from './list';
+import { Wrapper } from './wrapper-styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filters: '',
+  };
+
+  addContact = newContact => {
+    const isSame = this.state.contacts.find(
+      contact => contact.name === newContact.name
+    );
+
+    if (isSame) {
+      alert('This name already exist');
+    } else {
+      this.setState(prevState => ({
+        ...prevState,
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
+  };
+
+  changeFilter = evt => {
+    this.setState(prevState => ({
+      ...prevState,
+      filters: evt.target.value,
+    }));
+  };
+
+  deleteElementsOfList = contact => {
+    console.log(contact);
+
+    const newContactList = this.state.contacts.filter(
+      item => item.id !== contact.id
+    );
+    console.log(newContactList);
+
+    this.setState(prevState => ({
+      ...prevState,
+      contacts: newContactList,
+    }));
+  };
+
+  render() {
+    console.log(this.state);
+
+    const filteredList = this.state.contacts.filter(item =>
+      item.name.toLowerCase().includes(this.state.filters.toLowerCase())
+    );
+
+    return (
+      <Wrapper>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter changeFilter={this.changeFilter} />
+        <ContactList
+          contacts={filteredList}
+          deleteElementsOfList={this.deleteElementsOfList}
+        />
+      </Wrapper>
+    );
+  }
+}
